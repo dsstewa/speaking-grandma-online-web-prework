bca7a8b26bb79a2676baaf1a2dd25bc109d503df
David Stewart <davidstewart@gmail.com>
2019-02-10 17:46:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,10 +5,10 @@ def speak_to_grandma(word)
   if word == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
   
- # elsif word == word.upcase
- #   puts "NO, NOT SINCE 1938!"
- # else
- #   puts "HUH?! SPEAK UP, SONNY!"
+ elsif word == word.upcase
+   "NO, NOT SINCE 1938!"
+  else
+ "HUH?! SPEAK UP, SONNY!"
   end
   
 end
